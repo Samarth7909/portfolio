683977a87859b8d29d0c94f95cb5c4948b3f43bd
Samarth Gupta <your-samarthgupta7909@gmail.com>
2026-04-18 23:20:17 +0530

    fix: correct hireMe.png import casing for Linux build

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link as ScrollLink } from 'react-scroll'
 import './intro.css'
 import bg from '../../assets/image.png'
-import btnImg from '../../assets/hireMe.png'
+import btnImg from '../../assets/hireme.png'
 
 
 
